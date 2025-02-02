832f8648d76fd227eeee34e9f8daefd1f71a2b02
SeungJaeOh <ohsj3781@gmail.com>
2025-02-03 03:23:15 +0900

    feat: change Login->User minor changes

--- User/server.js
+++ User/server.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const cors = require('cors');
 
 // Load environment variables
 require('dotenv').config();
+
 const port=process.env.LOGIN_PORT;
 
 app.get('/',(req,res)=>{
